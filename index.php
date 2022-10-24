de4d7b01e087b2743eb49dab8d5171bbe59b34bf
Aprialdilubis <aprialdilubis999@gmail.com>
2022-10-24 10:57:42 +0700

    belajar php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello Word!<br><br><br>";
+
+$nama = "M. ApriAldi Lubis";
+$umur = 21;
+
+echo "Nama saya <strong> $nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Apri";
+$umurAdik = 15;
+
+$selisihumur = $umur-$umurAdik;
+
+echo "Nama Adik saya $namaAdik,adik saya berusia $umurAdik tahun.<br>";
+echo "<strong>Selisih</strong> usia saya dan adik saya adalah $selisihumur tahun."
+
+?>
